9716d834edecaa29dba92924b49cc9991958b9ae
ShivangiAsthanaAccolite <shivangi.asthana@accolitedigital.com>
2024-08-27 17:19:59 +0530

    first commit to demonstrate use of the published library from React component library

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@shivangiasthanaaccolite/template-react-component-library";
+import React from "react";
+
+function App() {
+  return <Button label="Hello world!" />;
+}
+
+export default App;
